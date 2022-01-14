c935eaa33c6030faa66683959bca625fc243cab3
suzakutakumi <suzakutakumi3@gmail.com>
2022-01-15 00:26:27 +0900

    moveのAPIを実装

---
+++ backend/move/move.go
@@ -0,0 +1,32 @@
+package move
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type _Move struct {
+	Number   int  `form:"number" json:number`
+	PlayerId int  `json:playerId`
+	IsMove   bool `json:isMove`
+	GroupId  int  `json:groupId`
+	X        int  `json:x`
+	Y        int  `json:y`
+}
+
+var move map[int]*_Move = map[int]*_Move{0: &_Move{0, 0, false, 0, 0, 0}}
+
+func PostMoveGroup(ctx *gin.Context) {
+	var num _Move
+	ctx.BindJSON(move[num.Number])
+	move[num.Number].IsMove = true
+	ctx.Status(http.StatusOK)
+}
+
+func GetMoveGroup(ctx *gin.Context) {
+	var num _Move
+	ctx.BindQuery(&num)
+	ctx.JSON(http.StatusOK, move[num.Number])
+	move[num.Number].IsMove = false
+}
