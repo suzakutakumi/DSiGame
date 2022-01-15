4092a70405023b5ab9654c2c22cf7b6a83837c65
suzakutakumi <suzakutakumi3@gmail.com>
2022-01-15 11:49:05 +0900

    POSTの作成&バグ修正

--- DSiGame/Assets/Scripts/HTTPRequest.cs
+++ DSiGame/Assets/Scripts/HTTPRequest.cs
@@ -9,6 +9,10 @@ public class HTTPRequest:MonoBehaviour
     {
         StartCoroutine(GETCoroutine(url));
     }
+    public void POST(string url,string body)
+    {
+        StartCoroutine(POSTCoroutine(url,body));
+    }
     private IEnumerator GETCoroutine(string url)
     {
         UnityWebRequest req = UnityWebRequest.Get(url);
@@ -24,6 +28,24 @@ public class HTTPRequest:MonoBehaviour
         else
         {
             result = req.downloadHandler.text;
+        }
+    }
+
+    private IEnumerator POSTCoroutine(string url,string body)
+    {
+        byte[] postData = System.Text.Encoding.UTF8.GetBytes(body);
+        var req = new UnityWebRequest(url, "POST");
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(postData);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+        yield return req.SendWebRequest();
+        if (req.isNetworkError)
+        {
+            result = req.error;
+        }
+        else if (req.isHttpError)
+        {
+            result = req.error;
         }
     }
 }
--- DSiGame/Assets/Scripts/HTTPRequestTest.cs
+++ DSiGame/Assets/Scripts/HTTPRequestTest.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class HTTPRequestTest : MonoBehaviour
 {
+    private MoveStruct move;
     [SerializeField]
     private HTTPRequest req;
     void Start()
     {
-        
+        move = new MoveStruct();
+        move.number = 0;
+        move.playerId = 1;
+        move.isMove = true;
+        move.groupId = 10;
+        move.x = 2;
+        move.y = 4;
     }
 
     // Update is called once per frame
@@ -16,11 +23,17 @@ public class HTTPRequestTest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            req.GET("https://google.com");
+            req.GET("http://localhost:8080/moveGroup?number="+move.number.ToString());
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            string body = JsonUtility.ToJson(move);
+            req.POST("http://localhost:8080/moveGroup",body);
         }
         if (req.result != null)
         {
-            Debug.Log(req.result);
+            var res=MoveStruct.Deserialize(req.result);
+            Debug.Log(JsonUtility.ToJson(res));
             req.result = null;
         }
     }
