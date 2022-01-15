f906a8dfca8ef3b3033fb08d0b231ea3ca774707
suzakutakumi <suzakutakumi3@gmail.com>
2022-01-16 05:27:04 +0900

    部屋の入室の作成

--- backend/main.go
+++ backend/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"backend/action"
 	"backend/move"
+	"backend/room"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	room.Init()
+
 	r := gin.Default()
 	r.POST("/moveGroup", move.PostMoveGroup)
 	r.GET("/moveGroup", move.GetMoveGroup)
 	r.POST("/action", action.PostAction)
 	r.GET("/action", action.GetAction)
+
+	r.GET("/room/create", room.CreateRoom)
+	r.POST("/room/enter", room.EnterRoom)
+	r.GET("/room/isStart", room.IsStart)
+	r.DELETE("/room", room.DeleteRoom)
 	r.Run()
 }
