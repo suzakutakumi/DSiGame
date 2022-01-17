e2ace33d04c08e1c26ea35eb13b4c04d206f88d6
id-isymhrk <isymhrk0608@gmail.com>
2022-01-18 00:49:46 +0900

    行動クラスが情報を持ってこれるように実装

--- DSiGame/Assets/Scripts/Actions/BaseActions.cs
+++ DSiGame/Assets/Scripts/Actions/BaseActions.cs
@@ -6,15 +6,12 @@ using UnityEngine;
 [System.Serializable]
 public class BaseActions
 {
+    public int number;
+    public int playerId;
+    public string whatAction;
     public int fromId=0;   //影響を与える側の群ID
     public int toId = 0;   //影響を受ける側の群ID
     public int newId = 0;  //新規群を作成する際に用いるID
     public int x = 0, z = 0;   //int型の座標
     public int moveCount = 0;  //移動させる群れ
-
-    public static BaseActions Deserialize(string json)
-    {
-        BaseActions user = JsonUtility.FromJson<BaseActions>(json);
-        return user;
-    }
 }
