6567979bcffe448b6c73381f4137f4af038f55f1
suzakutakumi <suzakutakumi3@gmail.com>
2022-01-17 23:39:47 +0900

    actionを直した

--- backend/action/action.go
+++ backend/action/action.go
@@ -6,46 +6,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-/*type _Attack struct {
-	GroupId      int `json:groupId`
-	EnemyGroupId int `json:enemyGroupId`
-	MyAttack     int `json:myAttack`
-	EnemyAttack  int `json:enemyAttack`
+type detailOfAction struct {
+	FromId    int `json:"fromId"`
+	ToId      int `json:"toId"`
+	NewId     int `json:"newId"`
+	X         int `json:"x"`
+	Z         int `json:"z"`
+	MoveCount int `json:"moveCount"`
 }
-
-type _Change struct {
-	FromId    int `json:fromId`
-	ToId      int `json:toId`
-	MoveCount int `json:moveCount`
-}
-
-type _Create struct {
-	FromId    int `json:fromId`
-	NewId     int `json:newId`
-	X         int `json:x`
-	Y         int `json:y`
-	MoveCount int `json:moveCount`
-}
-
-type _Evolution struct {
-	FromId    int `json:fromId`
-	NewId     int `json:newId`
-	X         int `json:x`
-	Y         int `json:y`
-	MoveCount int `json:moveCount`
-}*/
 
 type _Action struct {
-	Number     int         `form:"number" json:"number"`
-	PlayerId   int         `json:"playerId"`
-	WhatAction interface{} `json:"whatAction"`
-	Action     interface{} `json:"action"`
+	Number     int            `form:"number" json:"number"`
+	PlayerId   int            `json:"playerId"`
+	WhatAction interface{}    `json:"whatAction"`
+	Action     detailOfAction `json:"action"`
 }
 
-var action map[int]*_Action = map[int]*_Action{0: &_Action{0, 0, nil, nil}}
+var action map[int]*_Action = map[int]*_Action{0: {0, 0, nil, detailOfAction{0, 0, 0, 0, 0, 0}}}
 
 func NewRoom(num int) {
-	action[num] = &_Action{0, 0, nil, nil}
+	action[num] = &_Action{0, 0, nil, detailOfAction{0, 0, 0, 0, 0, 0}}
 }
 
 func DeleteRoom(num int) {
@@ -56,24 +36,6 @@ func PostAction(ctx *gin.Context) {
 	var num _Action
 	ctx.BindJSON(&num)
 	action[num.Number] = &num
-	/*switch action[num.Number].WhatAction {
-	case "attack":
-		var attack _Attack
-		ctx.BindJSON(&attack)
-		action[num.Number].Action = attack
-	case "change":
-		var change _Change
-		ctx.BindJSON(&change)
-		action[num.Number].Action = change
-	case "create":
-		var create _Create
-		ctx.BindJSON(&create)
-		action[num.Number].Action = create
-	case "evolution":
-		var evolution _Evolution
-		ctx.BindJSON(&evolution)
-		action[num.Number].Action = evolution
-	}*/
 	ctx.Status(http.StatusOK)
 }
 
