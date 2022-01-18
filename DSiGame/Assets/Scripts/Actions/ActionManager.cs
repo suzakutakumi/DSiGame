c98db2713d203a0894f6afd53a3e98e3da49bc36
id-isymhrk <isymhrk0608@gmail.com>
2022-01-18 17:20:48 +0900

    アクションマネージャーのスクリプトを作成

---
+++ DSiGame/Assets/Scripts/Actions/ActionManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class ActionManager : MonoBehaviour
+{
+    public List<GameObject> nowPlayerGroup = new List<GameObject>();
+    private IwasiCore fromIwasi;
+    private IwasiCore toIwasi;
+
+    private GenerateIwasi generate = new GenerateIwasi();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void getID(int fromId, int toId)
+    {
+        fromIwasi = nowPlayerGroup[fromId].GetComponent<IwasiCore>();
+        toIwasi= nowPlayerGroup[toId].GetComponent<IwasiCore>();
+    }
+
+    public void Attack()
+    {
+        toIwasi.Damaged(fromIwasi);
+    }
+
+    public void Exchange(int val)
+    {
+        fromIwasi.GiveOrTake(-val);
+        toIwasi.GiveOrTake(val);
+    }
+
+    public void Create(int val)
+    {
+        generate.Generate(fromIwasi.type, fromIwasi.x, fromIwasi.y);
+        fromIwasi.GiveOrTake(-val);
+    }
+
+    public void Evolution()
+    {
+
+    }
+
+    public void NoAction()
+    {
+
+    }
+}
