1201053b5bcd057de169e0d409dc786430c26459
Nito <niton984@gmail.com>
2022-01-24 16:56:27 +0900

    初期位置にアイテムをスポーンさせない

--- DSiGame/Assets/Scripts/Item/ItemGenerator.cs
+++ DSiGame/Assets/Scripts/Item/ItemGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Player;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,23 +9,28 @@ namespace Item
 {
     public class ItemGenerator : MonoBehaviour
     {
+        [SerializeField] private GameManager gameManager;
         [SerializeField, Range(0.0f, 1.0f)] private float density;
         [SerializeField] private List<BaseItem> itemList = new List<BaseItem>();
-        
+
         private int size_x;
         private int size_z;
+        private List<Vector2> sponePos = new List<Vector2>();
 
         void Start()
         {
             var createStage = GameObject.Find("PointCreateStage").GetComponent<CreateStage>();
-            size_x = createStage.GetX;
-            size_z = createStage.GetZ;
+            size_x = createStage.X;
+            size_z = createStage.Z;
             
+            FindSponePoint(gameManager.nowPlayerGroup);
+            FindSponePoint(gameManager.nowOpponentGroup);
+
             for (int i = 0; i < size_x; i++)
             {
                 for (int j = 0; j < size_z; j++)
                 {
-                    if(i < 2 && j == 0) continue; //スタート位置を無視
+                    if(sponePos.Contains(new Vector2(i * 10, j * 10))) continue; //スタート位置を無視
                     Generate(i * 10, j * 10);
                 }
             }
@@ -36,6 +42,15 @@ namespace Item
             if (Random.Range(0f, 1.0f) <= density * item.GetComponent<BaseItem>().Ratio)
             {
                 Instantiate(item, new Vector3(x, 1, z), Quaternion.identity, transform);
+            }
+        }
+
+        private void FindSponePoint(List<GameObject> iwasiList)
+        {
+            foreach (var group in iwasiList)
+            {
+                var core = group.GetComponent<IwasiCore>();
+                sponePos.Add(new Vector2(core.x, core.y));
             }
         }
 
